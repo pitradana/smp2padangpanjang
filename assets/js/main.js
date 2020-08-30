cddbfdfde637e00fd0181bf3af56fa28961bc94a
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2020-08-30 15:25:04 +0700

    Tenaga Pendidik

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,25 @@ $(document).ready(function() {
     loop: true,
     nav: true,
     items: 1,
+    dots: false,
+    margin:0,
     navText: [
       "<i class='fas fa-angle-left'><i>",
       "<i class='fas fa-angle-right'><i>"
     ],
     navContainer: "#slider-hero-nav",
+  });
+
+  $("#tenaga-pendidik-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    dots: false,
+    margin:30,
+    navText: [
+      "<i class='fas fa-angle-left'><i>",
+      "<i class='fas fa-angle-right'><i>"
+    ],
+    navContainer: "#slider-tools-1",
   });
 });
