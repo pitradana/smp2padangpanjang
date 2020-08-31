6cacb3b5168f361b29147f9bdf1340604b1e3655
Antonius Pitra Dana Arista <pitradana@users.noreply.github.com>
2020-09-01 04:05:06 +0700

    Galeri

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,30 @@ $(document).ready(function() {
     ],
     navContainer: "#slider-tools-1",
   });
+
+  $("#alumni-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 2,
+    dots: false,
+    margin:30,
+    navText: [
+      "<i class='fas fa-angle-left'><i>",
+      "<i class='fas fa-angle-right'><i>"
+    ],
+    navContainer: "#slider-tools-2",
+  });
+
+  $("#galeri-slider").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    dots: false,
+    margin:30,
+    navText: [
+      "<i class='fas fa-angle-left'><i>",
+      "<i class='fas fa-angle-right'><i>"
+    ],
+    navContainer: "#slider-tools-3",
+  });
 });
